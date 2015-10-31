d30162b1e95aa8212889eb07929c16072c340c3a
Dmytro Kotenko <d.kotenko@i.ua>
2015-10-31 12:15:02 +0200

    Added operators tests

---
+++ BrainConsoleAppTest/OperatorsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainConsoleAppTest
+{
+    [TestClass]
+    public class OperatorsTest
+    {
+        [TestMethod]
+        public void DoubleIncrementTest()
+        {
+            var testNumber = 0;
+            testNumber++;
+            ++testNumber;
+            Assert.AreEqual(2, testNumber);
+        }
+
+        [TestMethod]
+        public void ComplexDoubleIncrementTest()
+        {
+            var testNumber = 0;
+            var sufficsed = testNumber++;
+            Assert.AreEqual(0, sufficsed);
+
+            var otherTestNumber = 0;
+            var prefixed = ++otherTestNumber;
+            Assert.AreEqual(1, prefixed);
+        }
+    }
+}
