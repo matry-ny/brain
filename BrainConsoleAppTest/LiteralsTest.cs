0abcf0f2269f9e8e31b3d7a5518e0b830e5843c8
Dmytro Kotenko <d.kotenko@i.ua>
2015-10-31 11:32:05 +0200

    added data types tests

---
+++ BrainConsoleAppTest/LiteralsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainConsoleAppTest
+{
+    [TestClass]
+    public class LiteralsTest
+    {
+        const int GREED_LIMIT = 2;
+
+        [TestMethod]
+        public void replaceTest()
+        {
+            var message = "Test string";
+            message = message.Replace("string", "qwerty");
+            Assert.AreEqual(message, "Test qwerty");
+        }
+
+        [TestMethod]
+        public void slashesTest()
+        {
+            var message = "C:\\file.txt";
+            Console.WriteLine(message);
+            //Debug.WriteLine(message);
+
+            var othetString = @"C:\files\test.txt";
+            Assert.AreEqual("C:\\files\\test.txt", othetString);
+            Console.WriteLine(othetString);
+        }
+
+        [TestMethod]
+        public void constantsTest()
+        {
+            for (int step = 0; step <= GREED_LIMIT; step++)
+            {
+                Console.WriteLine("STEP #" + step + "\r\n");
+            }
+        }
+    }
+}
---
+++ BrainConsoleAppTest/TypeConversionsTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainConsoleAppTest
+{
+    [TestClass]
+    public class TypeConversionsTest
+    {
+        private long longArgument = 1;
+        private short shortArgument = 2;
+        private int intArgument = 3;
+
+        [TestMethod]
+        public void AutomaticExpressionConversionTest()
+        {
+            var calculationResult = this.intArgument + this.shortArgument + this.longArgument;
+            Assert.AreSame(longArgument.GetType(), calculationResult.GetType());
+        }
+
+        [TestMethod]
+        public void ManualExpressionConversionTest()
+        {
+            var calculationResult = this.intArgument + this.shortArgument + (short)this.longArgument;
+            Assert.AreSame(intArgument.GetType(), calculationResult.GetType());
+        }
+
+        [TestMethod]
+        public void IncompatibleExpressionConversionTest()
+        {
+            int x = 1;
+            int y = 2;
+
+            var calculationResult = x / y;
+            Console.WriteLine(calculationResult);
+        }
+    }
+}
