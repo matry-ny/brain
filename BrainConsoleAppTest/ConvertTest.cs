91ded57d4bb4543e2935251a7e4a73f13c3c1ec4
Dmytro Kotenko <d.kotenko@i.ua>
2015-11-05 21:08:25 +0200

    Added new tests

---
+++ BrainConsoleAppTest/ConvertTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainConsoleAppTest
+{
+    [TestClass]
+    public class ConvertTest
+    {
+        [TestMethod]
+        public void ToInt32Test()
+        {
+            var startNumber = 123.3m;
+            var culture = CultureInfo.CurrentCulture;
+            var simpleString = startNumber.ToString(culture);
+
+            var value = Convert.ToDecimal(simpleString, culture);
+            Assert.AreEqual(startNumber, value);
+        }
+    }
+}
