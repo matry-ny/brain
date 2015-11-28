5fa6ccc42d76f55a4f2d2c1d31a97d83a5c169ca
Dmytro Kotenko <d.kotenko@i.ua>
2015-11-28 12:20:09 +0200

    Added method params tests

---
+++ BrainConsoleApp/OOP/Methods/StringFormatter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BrainConsoleApp.OOP.Methods
+{
+    public class StringFormatter
+    {
+        private const string Version = "0.1";
+        public const string Square = "Square";
+        public const string Rectagle = "Rectagle";
+        public const string FigureFormat = "{0} ({1}x{2})";
+
+        public string GetVersion()
+        {
+            return Version;
+        }
+
+        public string MakeInt(string format, int value)
+        {
+            return String.Format(format, value);
+        }
+
+        public string MakeFigure(int height, int width)
+        {
+            var figure = height == width ? Square : Rectagle;
+            return String.Format(FigureFormat, figure, width, height);
+        }
+
+        public string MakeIntegersLine(string intFormat, char separator = ';', params int[] integers)
+        {
+            string result = "";
+            foreach (var item in integers)
+            {
+                result += String.Format(intFormat, item) + separator;
+            }
+            return result.Trim(separator);
+        }
+    }
+}
---
+++ BrainConsoleAppTest/OOP/Methods/StringFormatterTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BrainConsoleApp.OOP.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainConsoleAppTest.OOP.Methods
+{
+    [TestClass]
+    public class StringFormatterTest
+    {
+        [TestMethod]
+        public void VersionTest()
+        {
+            var result = (new StringFormatter()).GetVersion();
+            Assert.IsFalse(String.IsNullOrEmpty(result));
+        }
+
+        [TestMethod]
+        public void MakeIntTest()
+        {
+            var result = (new StringFormatter()).MakeInt("---{0}---", 43);
+            string expected = "---43---";
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void MakeFigureTest()
+        {
+            var formatter = new StringFormatter();
+           
+            var w = 54;
+            var h = 33;
+            var result = formatter.MakeFigure(h, w);
+            Assert.AreEqual(result, String.Format(StringFormatter.FigureFormat, StringFormatter.Rectagle, w, h));
+
+            w = 10;
+            h = 10;
+            result = formatter.MakeFigure(width: w, height: h);
+            Assert.AreEqual(result, String.Format(StringFormatter.FigureFormat, StringFormatter.Square, w, h));
+        }
+
+        [TestMethod]
+        public void MakeIntegersLineTest()
+        {
+            var formatter = new StringFormatter();
+
+            var result = formatter.MakeIntegersLine("[{0}]", ',', 1, 44, 66, 982, 666);
+            Assert.AreEqual("[1],[44],[66],[982],[666]", result);
+
+            result = formatter.MakeIntegersLine("{0}");
+            Assert.IsTrue(String.IsNullOrEmpty(result));
+
+            result = formatter.MakeIntegersLine("{0}", '|');
+            Assert.IsTrue(String.IsNullOrEmpty(result));
+
+            result = formatter.MakeIntegersLine("{0}", integers: new []{0, 1, 2, 3});
+            Assert.AreEqual("0;1;2;3", result);
+        }
+    }
+}
